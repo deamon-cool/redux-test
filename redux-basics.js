87021216015f2cb2981f787cffd0f07714be5b58
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-15 19:45:45 +0200

    added subscription that invoking function after each state change

--- redux-basics.js
+++ redux-basics.js
@@ -28,6 +28,11 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState())
 
+//Subscription
+store.subscribe(() => {
+    console.log('Subscription', store.getState());
+})
+
 // Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
