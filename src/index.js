8dff0c9657fb4b88f47d22831c3c130d56832ec5
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-15 19:49:36 +0200

    added redux create store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
