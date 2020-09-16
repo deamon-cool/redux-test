acb9943652680517c760f31c25f1cad4542b8597
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-16 18:29:09 +0200

    added ...state spread object because of new state value in reducer 'results'. Thanks to spreading object the final state will be updated only with the value that has changed

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,19 +7,28 @@ const reducer = (state = initState, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
+                ...state,
                 counter: state.counter + 1
             };
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             };
         case 'ADD_VALUE':
             return {
+                ...state,
                 counter: state.counter + action.value
             };
         case 'SUBTRACT_VALUE':
             return {
+                ...state,
                 counter: state.counter - action.value
+            };
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat(state.counter)
             };
         default:
             return state;
