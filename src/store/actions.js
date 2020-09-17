778cf7a8efdebf18228d31ba05490a302c1f4e48
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-17 17:35:56 +0200

    added outsourcing all reducer action names. It helps avoiding misspelling

---
+++ src/store/actions.js
@@ -0,0 +1,6 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const ADD_VALUE = 'ADD_VALUE';
+export const SUBTRACT_VALUE = 'SUBTRACT_VALUE';
+export const STORE_RESULT = 'STORE_RESULT';
+export const DELETE_RESULT = 'DELETE_RESULT';
