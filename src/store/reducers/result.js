33a318f052aa7609f45846469e4c3553878e2f77
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-17 18:48:11 +0200

    updated result reducer with action value

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -9,7 +9,7 @@ const reducer = (state = initState, action) => {
         case actionTypes.STORE_RESULT:
             return {
                 ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter })
+                results: state.results.concat({ id: new Date(), value: action.result })
             };
         case actionTypes.DELETE_RESULT:
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
