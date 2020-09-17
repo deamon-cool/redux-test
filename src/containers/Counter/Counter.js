ef9f6e59ccb02ce9c6c3e94a2fd5f38f784d6420
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-17 18:49:32 +0200

    passed appropriate value as an argument to the function

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -18,7 +18,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.onAddValue} />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractValue} />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.counterValue)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(storeResult => (
                         <li key={storeResult.id}
