fd5b63d31a97466365c9a683d09c23389e1816ea
daemon-cool <damian.damian.tofik@gmail.com>
2020-09-17 18:18:45 +0200

    deleted

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -6,26 +6,6 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = (action, value) => {
-        switch (action) {
-            case 'inc':
-                this.setState((prevState) => { return { counter: prevState.counter + 1 } })
-                break;
-            case 'dec':
-                this.setState((prevState) => { return { counter: prevState.counter - 1 } })
-                break;
-            case 'add':
-                this.setState((prevState) => { return { counter: prevState.counter + value } })
-                break;
-            case 'sub':
-                this.setState((prevState) => { return { counter: prevState.counter - value } })
-                break;
-        }
-    }
 
     render() {
         console.log('render')
